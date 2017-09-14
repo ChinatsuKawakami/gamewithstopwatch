c5a963b0f379511c75574f311623b247cbbbaaec
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-14 10:42:16 -0400

    0.0.3 edit main.js file to add the function to cause the event when the user pushes start button, the stop watch works.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 /*
 Author: Chinatsu Kawakami
 Create Date: 2017-09-13
-Modified Date: 2017-09-13
+Modified Date: 2017-09-14
 Description : This is the main.js file
 */
+
+
+(funtion (){
+'use strict';
+
+var start = doccument.getElementById('start');
+
+var stop = doccument.getElementById('stop');
+
+var result = doccument.getElementById('result');
+
+var startTime;
+
+start.addEventListener('click',function(){
+   startTime = Date.now();
+   
+});
+
+
+})();
